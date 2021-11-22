2797db29f1c811f797bded072e526795c5d6357e
Despgithub <rzmudari@gmail.com>
2021-11-22 08:47:00 +0400

    Исправленные задания

--- sandbox/src/main/java/ru/stqa/pft/sanbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Point.java
@@ -1,35 +1,21 @@
 package ru.stqa.pft.sanbox;
 
 public class Point {
-    double x, y, x1, y1, x2, y2;
+    double x, y;
 
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
+    public Point(double x1, double y1) {
+        this.x = x1;
+        this.y = y1;
     }
 
-    public Point(double x1, double y1, double x2, double y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-    }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-    }
-
-    public double distance() {
-        return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
     }
 
     public static void main(String[] args) {
         Point p1 = new Point(8, -1);
         Point p2 = new Point(4, 2);
-        System.out.println("Расстояние между точками, вычисленное функцией = " + distance(p1, p2));
-
-        Point d = new Point(8, -1, 4, 2);
-        System.out.println("Расстояние между точками, вычисленное методом = " + d.distance());
+        System.out.println("Расстояние между точками p1 и p2 = " + p1.distance(p2));
     }
 }
 
--- sandbox/src/test/java/ru/stqa/pft/sanbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sanbox/PointTests.java
@@ -6,12 +6,23 @@ import org.testng.annotations.Test;
 public class PointTests {
 
     @Test
-    public void testPoint() {
-        Point d = new Point(8, -1, 4, 2);
-        Assert.assertEquals(d.distance(), 5.0);
-        Point d1 = new Point(2, -5, -4, 3);
-        Assert.assertEquals(d1.distance(), 10.0);
-        Point d2 = new Point(2, 6, -5, 6);
-        Assert.assertEquals(d2.distance(), 7.0);
+    public void testPoint1() {
+        Point p1 = new Point(8, -1);
+        Point p2 = new Point(4, 2);
+        Assert.assertEquals(p1.distance(p2), 5.0);
+    }
+
+    @Test
+    public void testPoint2() {
+        Point p1 = new Point(2, -5);
+        Point p2 = new Point(-4, 3);
+        Assert.assertEquals(p1.distance(p2), 10.0);
+    }
+
+    @Test
+    public void testPoint3() {
+        Point p1 = new Point(2, 6);
+        Point p2 = new Point(-5, 6);
+        Assert.assertEquals(p1.distance(p2), 7.0);
     }
 }
