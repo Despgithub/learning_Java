2368eaa041e9a0713e5bb77b6a8ee6b0e4ec766b
Despgithub <rzmudari@gmail.com>
2021-11-27 03:06:11 +0400

    Убраны лишние действия, добавлен метод выхода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -23,10 +23,8 @@ public class GroupCreationTests {
     }
 
     private void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//input[@value='Login']")).click();
@@ -39,27 +37,29 @@ public class GroupCreationTests {
         fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
         submitGroupCreation();
         returnToGroupPage();
+        logout();
     }
 
-    private void returnToGroupPage() {
+    private void logout() {
         wd.findElement(By.linkText("Logout")).click();
     }
 
-    private void submitGroupCreation() {
+    private void returnToGroupPage() {
         wd.findElement(By.linkText("group page")).click();
     }
 
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+
     private void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
         wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
         wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
         wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-        wd.findElement(By.name("submit")).click();
     }
 
     private void initGroupCreation() {
