e74cf7af0d8ecf64c255ec505cd201a554773b54
Despgithub <rzmudari@gmail.com>
2021-11-26 13:52:25 +0400

    Исправление по д/з - класс вынесен в отдельный файл

--- sandbox/src/main/java/ru/stqa/pft/sanbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Point.java
@@ -17,13 +17,4 @@ public class Point {
     }
 }
 
-class PointStart {
-    public static void main(String[] args) {
-        Point p1 = new Point(8, -1);
-        Point p2 = new Point(4, 2);
-        System.out.println("Расстояние между точками p1 и p2, вычисленное функцией = " + Point.distance(p1, p2));
-        System.out.println("Расстояние между точками p1 и p2, вычисленное методом = " + p1.distance(p2));
-    }
-}
-
 
