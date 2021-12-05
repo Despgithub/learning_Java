c90b44ebcfd6a69e0d3ce14eae50fcaeac87dfc6
Despgithub <rzmudari@gmail.com>
2021-12-05 16:02:23 +0400

    Оптимизация заполнения форм: оставляем не изменяющися значения в полях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -10,7 +10,7 @@ public class ContactCreationTests extends TestBase {
         app.getNavigationHelper().gotoContactCreationPage();
         app.getContactHelper().fillContactForm(new ContactData("Ostap", "Suleiman Berta Maria", "Bender",
                 "0$ia", "The great combinator", "Horns and hooves", "Russia,Moscow, " +
-                "Old Arbat street 13,1", "4950000000", "+79111111111", "0$ia@bender.ru",
+                "Old Arbat street 13,1", null, "+79111111111", "0$ia@bender.ru",
                 "www.horns&hooves.com"));
         app.getContactHelper().submitContactCreation();
         app.getNavigationHelper().gotoHomePage();
