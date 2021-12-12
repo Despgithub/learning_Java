8b7b57adaea990b1554f12db99814a8b325d7d9c
Despgithub <rzmudari@gmail.com>
2021-12-12 20:39:39 +0400

    Сортировка списков контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -35,7 +35,7 @@ public class ContactData {
 
     public ContactData(String firstname, String middlename, String lastname, String nickname, String title,
                        String company, String address, String homephone, String mobile, String email, String group, String homepage) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.firstname = firstname;
         this.middlename = middlename;
         this.lastname = lastname;
@@ -122,16 +122,16 @@ public class ContactData {
 
         ContactData that = (ContactData) o;
 
-        if (id != that.id) return false;
         if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
         return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+        int result = firstname != null ? firstname.hashCode() : 0;
         result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
         return result;
     }
+
+
 }
