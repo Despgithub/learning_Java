0dc6967867012cbc540daed90eb8b9fb097a7379
Despgithub <rzmudari@gmail.com>
2021-12-17 13:52:00 +0400

    Переработан тест создания контакта для массового запуска

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -11,6 +11,7 @@ public class ContactCreationTests extends TestBase {
 
     @Test(enabled = true, invocationCount = 1)
     public void testContactCreation() {
+        app.getNavigationHelper().gotoHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().gotoContactCreationPage();
         ContactData contact = new ContactData("Ostap", "Suleiman Berta Maria", "Bender",
