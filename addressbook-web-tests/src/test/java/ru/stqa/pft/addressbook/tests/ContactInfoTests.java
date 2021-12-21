35a53c55b991eed46d9eec5ac41927b2d418ac02
Despgithub <rzmudari@gmail.com>
2021-12-21 17:30:41 +0400

    + пара правок по предупреждениям

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInfoTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInfoTests.java
@@ -4,8 +4,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -34,8 +34,8 @@ public class ContactInfoTests extends TestBase {
     }
 
     private String mergePhones(ContactData contact) {
-        return Arrays.asList(contact.getHomePhone(), contact.getMobile(), contact.getWorkPhone())
-                .stream().filter((s) -> !s.equals("")).map(ContactInfoTests::cleanedPhones)
+        return Stream.of(contact.getHomePhone(), contact.getMobile(), contact.getWorkPhone()).
+                filter((s) -> !s.equals("")).map(ContactInfoTests::cleanedPhones)
                 .collect(Collectors.joining("\n"));
     }
 
@@ -44,8 +44,8 @@ public class ContactInfoTests extends TestBase {
     }
 
     private String mergeEmails(ContactData contact) {
-        return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
-                .stream().filter((e) -> !e.equals("")).map(ContactInfoTests::cleanedEmails)
+        return Stream.of(contact.getEmail(), contact.getEmail2(), contact.getEmail3()).
+                filter((e) -> !e.equals("")).map(ContactInfoTests::cleanedEmails)
                 .collect(Collectors.joining("\n"));
     }
 
