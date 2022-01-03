7c5dd49680189f2f56dcdfb380377b9928fcaccf
Desp <rzmudari@gmail.com>
2022-01-03 16:26:24 +0400

    Отключаемые проверки UI для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -9,10 +9,16 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -40,6 +46,15 @@ public class TestBase {
     public void logTestStop(Method m, Object[] p) {
         logger.info("Остановим тест " + m.getName() + " с параметрами " + Arrays.asList(p));
 
+    }
+
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream().map((g) -> new GroupData()
+                    .withId(g.getId()).withName(g.getName())).collect(Collectors.toSet())));
+        }
     }
 
 }
