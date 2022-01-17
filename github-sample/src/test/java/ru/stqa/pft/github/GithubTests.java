5795d38cdaf6741583423cc187cb00a69df759c6
Desp <rzmudari@gmail.com>
2022-01-17 17:39:55 +0400

    Обновлен токен гитхаба

--- github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -10,7 +10,7 @@ public class GithubTests {
 
     @Test
     public void testCommits() throws IOException {
-        Github github = new RtGithub("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");
+        Github github = new RtGithub("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");
         RepoCommits commits = github.repos().get(new Coordinates.Simple("Despgithub", "learning_Java")).commits();
         for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
             System.out.println(new RepoCommit.Smart(commit).message());
